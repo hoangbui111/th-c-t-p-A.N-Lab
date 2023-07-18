fab9a146b09e87f9ee3d13d306e59f5d43892a44
hoangbui111 <hoangfacker687@gmail.com>
2023-07-18 15:03:13 +0700

    sample message

--- Workspace/tiktok-api/app/Http/Controllers/Profilecontroller.php
+++ Workspace/tiktok-api/app/Http/Controllers/Profilecontroller.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,30 +9,45 @@ class ProfileController extends Controller
 {
     public function index()
     {
-        $user = User::find(1); // Thay đổi số 1 thành ID của người dùng cần hiển thị thông tin
+        $userId = Auth::id(); 
+        $user = User::find($userId); 
 
         return view('profile')->with('user', $user);
     }
 
     public function update(Request $request)
-    {   
-        User::truncate();
-        $user = new User(); // Lấy thông tin người dùng đang đăng nhập
+    {
+        $userId = Auth::id(); 
+        $user = User::find($userId); 
+
+        
+        if (!$user) {
+            return redirect('/profile')->with('error', 'Người dùng không tồn tại.');
+        }
+
+        
+        $validator = Validator::make($request->all(), [
+            'username' => 'required',
+            'email' => 'required|email',
+            'birthdate' => 'required|date',
+            'address' => 'nullable',
+            'bio' => 'nullable',
+            'hobby' => 'nullable',
+        ]);
+
+      
+        if ($validator->fails()) {
+            return redirect('/profile')->withErrors($validator)->withInput();
+        }
+
+      
         $user->username = $request->input('username');
         $user->email = $request->input('email');
         $user->birthdate = $request->input('birthdate');
         $user->address = $request->input('address');
         $user->bio = $request->input('bio');
-        // Validate dữ liệu được gửi từ form
-        // $validator = Validator::make($request->all(), [
-        //     'username' => 'required',
-        //     'email' => 'required|email',
-        //     'birthdate' => 'required|date',
-        //     'address' => 'nullable',
-        //     'bio' => 'nullable',
-        //     'hobby' => 'nullable',
-        // ]);
         $user->save();
+
         return redirect('/profile')->with('success', 'Hồ sơ đã được cập nhật.');
     }
 }
--- Workspace/tiktok-api/resources/views/profile.blade.php
+++ Workspace/tiktok-api/resources/views/profile.blade.php
@@ -1,58 +1,10 @@
-<?php
-    // Các biến kết nối CSDL
-    $servername = "localhost";
-    $username = "root";
-    $password = "Tronghoang1*";
-    $dbname = "tiktok";
-
-    // Kết nối đến CSDL
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Kết nối thất bại: " . $conn->connect_error);
-    }
-
-    // Kiểm tra nút "Lưu thay đổi" được nhấn hay chưa
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Lấy dữ liệu từ form
-        $username = $_POST["username"];
-        $email = $_POST["email"];
-        $birthdate = $_POST["birthdate"];
-        $address = $_POST["address"];
-        $bio = $_POST["bio"];
-        $hobby = $_POST["hobby"];
-
-        // Câu truy vấn SQL để cập nhật thông tin người dùng
-        $sql = "UPDATE users SET username='$username', email='$email', birthdate='$birthdate', address='$address', bio='$bio', hobby='$hobby' WHERE id = 1"; // Thay đổi 'users' và 'id' tương ứng với cấu trúc CSDL của bạn
-
-        // Thực thi truy vấn và kiểm tra kết quả
-        if ($conn->query($sql) === TRUE) {
-            echo "Cập nhật thông tin hồ sơ thành công.";
-        } else {
-            echo "Lỗi khi cập nhật hồ sơ: " . $conn->error;
-        }
-    }
-
-    // Truy vấn dữ liệu người dùng từ CSDL
-    $sql = "SELECT * FROM users WHERE id = 1"; // Thay đổi 'users' và 'id' tương ứng với cấu trúc CSDL của bạn
-    $result = $conn->query($sql);
-
-    // Kiểm tra kết quả truy vấn và gán dữ liệu người dùng vào biến $user
-    if ($result->num_rows > 0) {
-        $user = $result->fetch_assoc();
-    } else {
-        echo "Không tìm thấy người dùng.";
-    }
-
-    // Đóng kết nối CSDL
-    $conn->close();
-    ?>
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Hồ sơ người dùng</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-        <style>
-            body {
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hồ sơ người dùng</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <style>
+         body {
                 background-color: #f5f5f5;
             }
             .card {
@@ -88,105 +40,114 @@
             .edit-button {
                 text-align: right;
             }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="profile-header">
-                <h1>Hồ sơ người dùng</h1>
-                <p>Xem và chỉnh sửa thông tin hồ sơ của bạn</p>
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="profile-header">
+            <h1>Hồ sơ người dùng</h1>
+            <p>Xem và chỉnh sửa thông tin hồ sơ của bạn</p> 
+        </div>
+
+        <div class="card">
+            @if (Session::has('success'))
+                <div class="alert alert-success">
+                    {{ Session::get('success') }}
+                </div>
+            @endif
+
+            <img class="avatar" src="https://kenh14cdn.com/6IlWjk2rcfxxcccccccccccca3bMNy/Image/2012/03/120317tekthu-10_83cbe.jpg" alt="Avatar" required>
+            <div class="container">
+                <div class="edit-button">
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal">Edit</button>
+                </div>
+                <h3><b>Tên người dùng:</b> {{ $user['username'] }}</h3>
+                <p><strong>Email:</strong> {{ $user['email'] }}</p>
+                <p><strong>Ngày sinh:</strong> {{ $user['birthdate'] }}</p>
+                <p><strong>Địa chỉ:</strong> {{ $user['address'] }}</p>
+                <p><strong>Giới thiệu:</strong> {{ $user['bio'] }}</p> 
             </div>
-            <div class="card">
-                @if (Session::has('thong bao'))
-                    <div class="alert-alert-success">
-                        {{Session::get('thongbao')}}
+        </div>
+    </div>
+
+    <form action="{{ route('profile.update') }}" method="POST">
+        @csrf
+        @method('PUT')
+        <!-- Các trường dữ liệu và các phần tử khác -->
+        <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editModalLabel">Chỉnh sửa hồ sơ</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
-                @endif
-                <img class="avatar" src="https://kenh14cdn.com/6IlWjk2rcfxxcccccccccccca3bMNy/Image/2012/03/120317tekthu-10_83cbe.jpg" alt="Avatar" required>
-                <div class="container">
-                    <div class="edit-button">
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal">Edit</button>
+                    <div class="modal-body">
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="username">Tên người dùng</label>
+                            <input type="text" class="form-control" id="username" name="username" value="{{ $user['username'] }}" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" value="{{ $user['email'] }}" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="birthdate">Ngày sinh</label>
+                            <input type="date" class="form-control" id="birthdate" name="birthdate" value="{{ $user['birthdate'] }}" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Địa chỉ</label>
+                            <input type="text" class="form-control" id="address" name="address" value="{{ $user['address'] }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="bio">Giới thiệu</label>
+                            <textarea class="form-control" id="bio" name="bio">{{ $user['bio'] }}</textarea>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
+                            <button type="submit" class="btn btn-success mt-2">Lưu thay đổi</button>
+                        </div>
                     </div>
-                    <h3><b>Tên người dùng:</b> <?php echo $user['username']; ?></h3>
-                    <p><strong>Email:</strong> <?php echo $user['email']; ?></p>
-                    <p><strong>Ngày sinh:</strong> <?php echo $user['birthdate']; ?></p>
-                    <p><strong>Địa chỉ:</strong> <?php echo $user['address']; ?></p>
-                    <p><strong>Giới thiệu:</strong> <?php echo $user['bio']; ?></p> 
                 </div>
             </div>
         </div>
-    
-        
+    </form>
 
-        <form action="{{ route('profile.update') }}" method="POST">
-            @csrf
-            <!-- Các trường dữ liệu và các phần tử khác -->
-            <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="editModalLabel">Chỉnh sửa hồ sơ</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            @method('PUT')
-                            <div class="form-group">
-                                <label for="username">Tên người dùng</label>
-                                <input type="text" class="form-control" id="username" name="username" value="{{ $user['username'] }}" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" value="{{ $user['email'] }}" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="birthdate">Ngày sinh</label>
-                                <input type="date" class="form-control" id="birthdate" name="birthdate" value="{{ $user['birthdate'] }}" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="address">Địa chỉ</label>
-                                <input type="text" class="form-control" id="address" name="address" value="{{ $user['address'] }}">
-                            </div>
-                            <div class="form-group">
-                                <label for="bio">Giới thiệu</label>
-                                <textarea class="form-control" id="bio" name="bio">{{ $user['bio'] }}</textarea>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Hủy</button>
-                                <button type="submit" class="btn btn-success mt-2">Lưu thay đổi</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </form>
-        
-           
-            
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
         $(document).ready(function() {
+            $('#editModal').on('show.bs.modal', function() {
+                var form = $(this).find('form');
+                form.find('input[name="username"]').val('{{ $user['username'] }}');
+                form.find('input[name="email"]').val('{{ $user['email'] }}');
+                form.find('input[name="birthdate"]').val('{{ $user['birthdate'] }}');
+                form.find('input[name="address"]').val('{{ $user['address'] }}');
+                form.find('textarea[name="bio"]').val('{{ $user['bio'] }}');
+            });
+
             $('#profileForm').on('submit', function(event) {
                 event.preventDefault();
-                
+
                 $.ajax({
                     url: "{{ route('profile.update') }}",
-                    method: "PUT",
+                    method: "POST",
                     data: $(this).serialize(),
                     dataType: "json",
                     success: function(response) {
                         if (response.success) {
-                            // Cập nhật thành công
-                            // Có thể thêm thông báo thành công hoặc tác vụ sau khi cập nhật vào đây
+                            $('#editModal').modal('hide'); // Đóng modal
+                            location.reload(); // Tải lại trang sau khi cập nhật thành công
                         }
                     }
                 });
             });
         });
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     </script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    </body>
-    </html>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
